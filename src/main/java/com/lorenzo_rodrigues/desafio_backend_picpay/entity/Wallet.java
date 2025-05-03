195538f9892d07a9cc1bddd26a19532b4b57f1b7
Lorenzo Rodrigues <lorenzocostarodrigues10@gmail.com>
2025-05-03 13:45:50 -0300

    created Wallet entity

---
+++ src/main/java/com/lorenzo_rodrigues/desafio_backend_picpay/entity/Wallet.java
@@ -0,0 +1,36 @@
+package com.lorenzo_rodrigues.desafio_backend_picpay.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_wallets")
+@Getter
+@Setter
+@AllArgsConstructor
+public class Wallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String fullName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    @Enumerated(EnumType.STRING)
+    private WalletType walletType;
+
+}
