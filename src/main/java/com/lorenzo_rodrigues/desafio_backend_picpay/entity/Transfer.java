cff53800a294494d46787f023d74b389567ab676
Lorenzo Rodrigues <lorenzocostarodrigues10@gmail.com>
2025-05-03 15:31:28 -0300

    refactor : for simplicity decided to make entity Wallet id as Long , and entity Transfer as UUID.

--- src/main/java/com/lorenzo_rodrigues/desafio_backend_picpay/entity/Transfer.java
+++ src/main/java/com/lorenzo_rodrigues/desafio_backend_picpay/entity/Transfer.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "tb_transfer")
@@ -17,8 +18,8 @@ import java.time.LocalDateTime;
 public class Transfer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     @ManyToOne
     private Wallet payer;
--- src/main/java/com/lorenzo_rodrigues/desafio_backend_picpay/repository/TransferRepository.java
+++ src/main/java/com/lorenzo_rodrigues/desafio_backend_picpay/repository/TransferRepository.java
@@ -3,5 +3,7 @@ package com.lorenzo_rodrigues.desafio_backend_picpay.repository;
 import com.lorenzo_rodrigues.desafio_backend_picpay.entity.Transfer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransferRepository extends JpaRepository<Transfer,Long> {
+import java.util.UUID;
+
+public interface TransferRepository extends JpaRepository<Transfer, UUID> {
 }
