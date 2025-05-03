5255543377f46ae6d3b706a39dd248565356c8ce
Lorenzo Rodrigues <lorenzocostarodrigues10@gmail.com>
2025-05-03 15:44:09 -0300

    created a controller class and an endpoint to create a transaction

---
+++ src/main/java/com/lorenzo_rodrigues/desafio_backend_picpay/controller/TransferController.java
@@ -0,0 +1,22 @@
+package com.lorenzo_rodrigues.desafio_backend_picpay.controller;
+
+import com.lorenzo_rodrigues.desafio_backend_picpay.controller.dto.TransferRequest;
+import com.lorenzo_rodrigues.desafio_backend_picpay.entity.Transfer;
+import com.lorenzo_rodrigues.desafio_backend_picpay.service.TransferService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TransferController {
+    private final TransferService transferService;
+
+    public TransferController(TransferService transferService) {
+        this.transferService = transferService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Transfer> createTransfer (TransferRequest request){
+        transferService.transfer(request);
+    }
+}
