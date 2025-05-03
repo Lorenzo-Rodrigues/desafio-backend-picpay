dbeec4dcbab7de3cd41dcb323fa78096b08af5e7
Lorenzo Rodrigues <lorenzocostarodrigues10@gmail.com>
2025-05-03 12:47:40 -0300

    initial commit

---
+++ src/main/java/com/lorenzo_rodrigues/desafio_backend_picpay/DesafioBackendPicpayApplication.java
@@ -0,0 +1,12 @@
+package com.lorenzo_rodrigues.desafio_backend_picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendPicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendPicpayApplication.class, args);
+	}
+}
