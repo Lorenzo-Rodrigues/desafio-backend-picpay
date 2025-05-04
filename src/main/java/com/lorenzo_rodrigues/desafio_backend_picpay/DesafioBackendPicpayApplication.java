453e6115ce017301d41a2757e6496f173ba84586
Lorenzo Rodrigues <lorenzocostarodrigues10@gmail.com>
2025-05-04 10:24:57 -0300

    feign client annotation

--- src/main/java/com/lorenzo_rodrigues/desafio_backend_picpay/DesafioBackendPicpayApplication.java
+++ src/main/java/com/lorenzo_rodrigues/desafio_backend_picpay/DesafioBackendPicpayApplication.java
@@ -2,8 +2,10 @@ package com.lorenzo_rodrigues.desafio_backend_picpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DesafioBackendPicpayApplication {
 
 	public static void main(String[] args) {
