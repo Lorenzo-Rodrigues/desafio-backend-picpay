b96e15e71bd5cc8bd30afa0532d441828dfd8b92
Lorenzo Rodrigues <lorenzocostarodrigues10@gmail.com>
2025-05-04 10:30:25 -0300

    created an interface FeignClient to communicate with external authorization with a method that returns if the transaction is valid or not

---
+++ src/main/java/com/lorenzo_rodrigues/desafio_backend_picpay/client/AuthorizationClient.java
@@ -0,0 +1,15 @@
+package com.lorenzo_rodrigues.desafio_backend_picpay.client;
+
+import com.lorenzo_rodrigues.desafio_backend_picpay.client.dto.AuthorizationResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "AuthorizationExternalAPI",
+            url = "https://util.devi.tools/api/v2/authorize")
+public interface AuthorizationClient {
+
+    @GetMapping
+    AuthorizationResponse isAuthorize();
+
+    }
+
