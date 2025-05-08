154286a38637f5ad6b9684ef2a334d356173d15c
Lorenzo Rodrigues <lorenzocostarodrigues10@gmail.com>
2025-05-08 12:03:55 -0300

    created an empty constructor

--- src/main/java/com/lorenzo_rodrigues/desafio_backend_picpay/entity/Wallet.java
+++ src/main/java/com/lorenzo_rodrigues/desafio_backend_picpay/entity/Wallet.java
@@ -3,6 +3,7 @@ package com.lorenzo_rodrigues.desafio_backend_picpay.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Wallet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
