1e30101802f2e25426b6e7022fc47643b2a8a698
Lorenzo Rodrigues <lorenzocostarodrigues10@gmail.com>
2025-05-10 09:07:20 -0300

    created documentation with OpenAPI for endpoint and request schema

--- src/main/java/com/lorenzo_rodrigues/desafio_backend_picpay/controller/TransferController.java
+++ src/main/java/com/lorenzo_rodrigues/desafio_backend_picpay/controller/TransferController.java
@@ -3,6 +3,9 @@ package com.lorenzo_rodrigues.desafio_backend_picpay.controller;
 import com.lorenzo_rodrigues.desafio_backend_picpay.controller.dto.TransferRequest;
 import com.lorenzo_rodrigues.desafio_backend_picpay.entity.Transfer;
 import com.lorenzo_rodrigues.desafio_backend_picpay.service.TransferService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +22,11 @@ public class TransferController {
     }
 
     @PostMapping
+    @Operation(description = "Create transfer")
+    @ApiResponses(value = {
+                @ApiResponse(responseCode = "200", description = "Returns transfer with updated wallets "),
+                @ApiResponse(responseCode = "422", description = "Transfer is not valid or unauthorized")
+    })
     public ResponseEntity<Transfer> createTransfer (@RequestBody TransferRequest request){
 
         var transfer = transferService.createTransfer(request);
--- src/main/java/com/lorenzo_rodrigues/desafio_backend_picpay/controller/dto/TransferRequest.java
+++ src/main/java/com/lorenzo_rodrigues/desafio_backend_picpay/controller/dto/TransferRequest.java
@@ -1,6 +1,14 @@
 package com.lorenzo_rodrigues.desafio_backend_picpay.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.math.BigDecimal;
 
-public record TransferRequest(Long payer, Long payee, BigDecimal money) {
+public record TransferRequest(
+        @Schema(description = "default COMMON wallet with id 1",example = "1")
+        Long payer,
+        @Schema(description = "default MERCHANT wallet with id 2 ",example = "2")
+        Long payee,
+        @Schema(description = "default wallets starts with 1000 balance", example = "50")
+        BigDecimal money) {
 }
